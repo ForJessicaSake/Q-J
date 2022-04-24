80cdc0a4e76943aed2872b8d5720dc46e81ba4f2
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-04-24 15:41:37 +0100

    This is the first push to the remote repository

--- index.js
+++ index.js
@@ -2,10 +2,34 @@ const buttonClickEffect = document.querySelectorAll(".btn");
 for (let i = 0; i < buttonClickEffect.length; i++) {
     buttonClickEffect[i].addEventListener("click", function() {
         buttonClickEffect[i].style.backgroundColor = "green";
-
     });
 }
-const specialButtonEffect = document.getElementById("sp-btn").addEventListener("click", function() {
-    document.getElementById("sp-btn").style.backgroundColor = "yellow";
+const specialButtonEffect = document
+    .getElementById("sp-btn")
+    .addEventListener("click", function() {
+        document.getElementById("sp-btn").style.backgroundColor = "yellow";
+    });
 
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: "bottom",
+    distance: "100px",
+    duration: 2200,
+    delay: 200,
+    //     reset: true
 });
+
+sr.reveal(
+    ".main-container, .main-container-two, .main-container-three, .main-container-four, .account-container, .grid-footer-container, busy-2", {}
+);
+sr.reveal(
+    ".main-text, .#sp-btn, .text-box-two, .text-box-three, .text-box2-text, main-container-two", {
+        delay: 400,
+    }
+);
+sr.reveal(".account-container, #busy, .main-container-two", { interval: 700 });
+sr.reveal(
+    ".main-box, .input, .footertext, .main-text, .sp-btn, .text-box2-text,", {
+        interval: 700,
+    }
+);
