10380c15d97ca699d001449170dbc3a84b7faef5
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-04-24 02:42:36 +0100

    This is the first push to the remote repository

---
+++ index.js
@@ -0,0 +1,10 @@
+const buttonClickEffect=document.querySelectorAll(".btn");
+for (let i=0; i < buttonClickEffect.length; i++){
+    buttonClickEffect[i].addEventListener("click", function(){
+        buttonClickEffect[i].style.backgroundColor = "green";
+    });
+}
+const specialButtonEffect=document.getElementById("sp-btn").addEventListener("click",function(){
+    document.getElementById("sp-btn").style.backgroundColor ="yellow";
+
+});
