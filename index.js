5b4fdf101ce39e010dd448885b866f67d21b3b2a
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-04-24 12:47:28 +0100

    This is the first push to the remote repository

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const buttonClickEffect=document.querySelectorAll(".btn");
-for (let i=0; i < buttonClickEffect.length; i++){
-    buttonClickEffect[i].addEventListener("click", function(){
+const buttonClickEffect = document.querySelectorAll(".btn");
+for (let i = 0; i < buttonClickEffect.length; i++) {
+    buttonClickEffect[i].addEventListener("click", function() {
         buttonClickEffect[i].style.backgroundColor = "green";
+
     });
 }
-const specialButtonEffect=document.getElementById("sp-btn").addEventListener("click",function(){
-    document.getElementById("sp-btn").style.backgroundColor ="yellow";
+const specialButtonEffect = document.getElementById("sp-btn").addEventListener("click", function() {
+    document.getElementById("sp-btn").style.backgroundColor = "yellow";
 
 });
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
