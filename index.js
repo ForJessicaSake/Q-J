162a177d4c75eab837597974d90e0c6089ed45c4
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-04-24 14:54:32 +0100

    This is the first push to the remote repository

--- index.js
+++ index.js
@@ -9,20 +9,3 @@ const specialButtonEffect = document.getElementById("sp-btn").addEventListener("
     document.getElementById("sp-btn").style.backgroundColor = "yellow";
 
 });
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-};
-
-span.onclick = function() {
-    modal.style.display = "none";
-};
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
